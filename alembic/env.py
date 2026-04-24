f88c3bd03b904ae4c9344878318ab6968f9a7db2
Mr.Harry <harryprasad@student.telkomuniversity.ac.id>
2026-04-24 11:20:45 +0700

    SCRUM-16 membuat fitur pencarian ambulance berdasarkan lokasi saat ini

--- alembic/env.py
+++ alembic/env.py
@@ -32,6 +32,7 @@ from app.database import Base
 from app.models.user import User
 from app.models.message import Message
 from app.models.health_record import HealthRecord
+from app.models.ambulance import AmbulanceService
 
 target_metadata = Base.metadata
 
